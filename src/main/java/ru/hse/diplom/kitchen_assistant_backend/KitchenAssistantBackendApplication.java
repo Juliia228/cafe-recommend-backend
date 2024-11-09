8102a37c7e5ccfc3d11ede1c16309877370e826b
Juliia228 <miss-komarova2017@yandex.ru>
2024-11-09 16:16:42 +0300

    Initial Commit

---
+++ src/main/java/ru/hse/diplom/kitchen_assistant_backend/KitchenAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.diplom.kitchen_assistant_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenAssistantBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hse/diplom/kitchen_assistant_backend/KitchenAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.diplom.kitchen_assistant_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenAssistantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
