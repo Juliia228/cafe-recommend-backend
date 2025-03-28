e363cb9d842cd55fe0002719b49b185ab5a14828
Juliia228 <miss-komarova2017@yandex.ru>
2025-03-28 20:24:13 +0300

    Connect database

--- src/main/java/ru/hse/diplom/cafe_recommend_backend/CafeRecommendBackendApplication.java
+++ src/main/java/ru/hse/diplom/cafe_recommend_backend/CafeRecommendBackendApplication.java
@@ -2,8 +2,9 @@ package ru.hse.diplom.cafe_recommend_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CafeRecommendBackendApplication {
 
 	public static void main(String[] args) {
