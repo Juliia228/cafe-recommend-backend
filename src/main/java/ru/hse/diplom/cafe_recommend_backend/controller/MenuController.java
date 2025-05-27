bd70a96df994940e8e26ef19d47a97d8154f62bc
Juliia228 <miss-komarova2017@yandex.ru>
2025-05-27 16:31:05 +0300

    Connected to the database

--- src/main/java/ru/hse/diplom/cafe_recommend_backend/controller/MenuController.java
+++
@@ -1,9 +0,0 @@
-package ru.hse.diplom.cafe_recommend_backend.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
-public class MenuController {
-}
